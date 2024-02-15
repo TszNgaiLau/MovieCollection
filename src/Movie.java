759b86c4461ce1b98d352f27cbdc32b5536563a1
BT_2N2_25 <sample@bths.edu>
2024-02-15 13:58:43 -0500

    1:57

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Movie {
+import java.lang.reflect.Array;
+
+public class Movie {
+    private String title;
+    private String[] cast;
+    private String director;
+    private String overView;
+    private int runtime;
+    private double userRating;
+    public Movie(String title, String[] cast, String director, String overView, int runtime, double userRating) {
+        this.title = title;
+        this.director = director;
+        this.overView = overView;
+        this.runtime = runtime;
+        this.userRating = userRating;
+    }
+
 }
